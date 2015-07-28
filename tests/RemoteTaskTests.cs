96a119cf33c1246235840e64f7955ef37f12ffb2
Victor Milovanov <lostfreeman@gmail.com>
2015-07-27 21:01:51 -0700

    + test to ensure remote task can wrap sandboxed code

--- tests/RemoteTaskTests.cs
+++ tests/RemoteTaskTests.cs
@@ -3,21 +3,45 @@
     using System;
     using System.IO;
     using System.Reflection;
+    using System.Security;
+    using System.Security.Policy;
     using System.Threading.Tasks;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
     public class RemoteTaskTests
     {
+        static readonly Assembly testAssembly = Assembly.GetExecutingAssembly();
+
         [TestMethod]
         public async Task Sample()
         {
-            var testAssembly = Assembly.GetExecutingAssembly();
             var domainSetup = new AppDomainSetup
             {
                 ApplicationBase = Path.GetDirectoryName(testAssembly.Location),
             };
             var remoteDomain = AppDomain.CreateDomain("TestRemoteDomain", null, domainSetup);
+            await RunSample(remoteDomain);
+            AppDomain.Unload(remoteDomain);
+        }
+
+        [TestMethod]
+        public async Task SandboxedSample()
+        {
+            var domainSetup = new AppDomainSetup
+            {
+                ApplicationBase = Path.GetDirectoryName(testAssembly.Location),
+            };
+            var evidence = new Evidence();
+            evidence.AddHostEvidence(new Zone(SecurityZone.Internet));
+            var permissions = SecurityManager.GetStandardSandbox(evidence);
+            var remoteDomain = AppDomain.CreateDomain("TestSandboxDomain", null, domainSetup, permissions);
+            await RunSample(remoteDomain);
+            AppDomain.Unload(remoteDomain);
+        }
+
+        private static async Task RunSample(AppDomain remoteDomain)
+        {
             var sampleRemote = (SampleRemoteClass)remoteDomain.CreateInstanceAndUnwrap(
                 assemblyName: testAssembly.FullName,
                 typeName: typeof(SampleRemoteClass).FullName);
--- tests/SampleRemoteClass.cs
+++ tests/SampleRemoteClass.cs
@@ -3,7 +3,7 @@
     using System;
     using System.Threading.Tasks;
 
-    class SampleRemoteClass: MarshalByRefObject
+    public class SampleRemoteClass: MarshalByRefObject
     {
         public Task<int> Add(int a, int b)
         {
